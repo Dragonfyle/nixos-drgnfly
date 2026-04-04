1252892b75d9709271f5132628cd6676190d17df
Michal Rygorowicz <rygorowicz.michal@hotmail.com>
2026-04-04 12:51:55 +0200

    hyprland: add uwsm

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -1,7 +1,19 @@
 { pkgs, ... }:
 
 {
-  programs.hyprland.enable = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
+
+  programs.uwsm = {
+    enable = true;
+    waylandCompositors.hyprland = {
+      prettyName = "Hyprland";
+      comment = "Hyprland compositor managed by UWSM";
+      binPath = "/run/current-system/sw/bin/Hyprland";
+    };
+  };
 
   xdg.portal = {
     enable = true;
