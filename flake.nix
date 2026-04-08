d234a2fba57e89d615ef057f2b7cbd3e78fa2941
Michal Rygorowicz <rygorowicz.michal@hotmail.com>
2026-04-08 20:47:43 +0200

    iso: fix syntax

--- flake.nix
+++ flake.nix
@@ -12,48 +12,48 @@
 
   outputs = { nixpkgs, home-manager, ... }:
     {
-      nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+		nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
 
-        modules = [
-          ./hosts/desktop
+			modules = [
+			  ./hosts/desktop
 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.michal = import ./home;
-          }
-        ];
-      };
+			  home-manager.nixosModules.home-manager
+			  {
+				home-manager.useGlobalPkgs = true;
+				home-manager.useUserPackages = true;
+				home-manager.users.michal = import ./home;
+			  }
+			];
+		};
 
-      nixosConfigurations.drgnfly = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+		nixosConfigurations.drgnfly = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
 
-        modules = [
-          ./hosts/drgnfly
+			modules = [
+			  ./hosts/drgnfly
 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.michal = import ./home;
-          }
-        ];
-      };
+			  home-manager.nixosModules.home-manager
+			  {
+				home-manager.useGlobalPkgs = true;
+				home-manager.useUserPackages = true;
+				home-manager.users.michal = import ./home;
+			  }
+			];
+		};
+
+		nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [
+				"${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+				./hosts/iso
+				home-manager.nixosModules.home-manager
+				{
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.users.michal = import ./home;
+				}
+			];
+		};
     };
-
-	nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
-	  system = "x86_64-linux";
-	  modules = [
-		"${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-		./hosts/iso
-		home-manager.nixosModules.home-manager
-		{
-		  home-manager.useGlobalPkgs = true;
-		  home-manager.useUserPackages = true;
-		  home-manager.users.michal = import ./home;
-		}
-	  ];
-	};
 }
